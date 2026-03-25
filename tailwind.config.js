195ae43611253691d835d7e52ed000445a541044
Romil <luharromil@gmail.com>
2026-03-25 15:54:00 +0530

    update

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        brand: "#EE6A70",
+        dark: "#0f172a",
+        card: "#1e293b"
+      }
+    },
+  },
+  plugins: [],
+}
